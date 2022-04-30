95dd932a40c8bffbf1f87ecd3b16071126b39de4
Aniela00 <aniekoksa@gmail.com>
2022-04-30 20:53:48 +0100

    Add project files.

---
+++ Reverse words in a given string/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your sentence, remember to seperate each word with '.'");
+            string S = Console.ReadLine();
+            Stack<string> stack = new Stack<string>(); 
+            foreach (var c in S.Split('.'))
+            {
+                stack.Push(c);
+            }
+            foreach (var c in stack)
+            {
+                Console.Write(c + '.'); //tutaj wiem, że nie potrzebnie dodaję kropkę
+                                        //na końcu ostatniego słowa, ale nie wiem jak to obejść
+            }
+        }
+    }
+}
